8220f08b762d975ed69551566f9338d5f5d68d79
Stephen Castaneda <stephen.castaneda40@gmail.com>
2020-02-29 10:34:43 -0600

    shark tank displays

--- components/tank.js
+++ components/tank.js
@@ -1,9 +1,25 @@
 import utils from '../helpers/utils.js';
+import personData from '../helpers/data/personData.js'
 
 
 const tankBuilder = () => {
-    let domString = 'help';
-    utils.printToDom('tank', domString);
+
+    //import data from get alivepersons
+const persons = personData.getAlivePersons();
+let domString = ''
+domString += '<div class="">';
+domString += '<div class="card-header">SHARK TANK</div>';
+domString += '<ul class="list-group list-group-flush">';
+
+  //for each loop that gets each person loops through and prints them as list item. used temperaliteral in place of person's name in domstring
+persons.forEach((person) => {
+    domString += `<li class="list-group-item">${person.name}</li>`;
+});
+
+  domString += '</ul>';
+  domString += '</div>';
+
+utils.printToDom('tank', domString);
 
 };
 
